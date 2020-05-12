6212e473b86fda80c60df0c2db9edc563b34fc9d
Ma3querader <593661332@qq.com>
2020-05-13 00:47:45 +0800

    二级缓存完成

--- src/Test/java/com/lakers/dao/CacheTest.java
+++ src/Test/java/com/lakers/dao/CacheTest.java
@@ -24,12 +24,13 @@ public class CacheTest {
 
     private InputStream in;
     private SqlSession session;
+    private SqlSessionFactory factory;
 
     @Before
     public void init() throws IOException {
         in = Resources.getResourceAsStream("mybatis-config.xml");
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);
+        factory = builder.build(in);
         session = factory.openSession();
     }
 
@@ -43,6 +44,21 @@ public class CacheTest {
         mapper.updateUser(user);
         session.commit();
         User u2 = mapper.findById(2);
+        System.out.println(u);
+        System.out.println(u2);
+    }
+
+    @Test
+    public void testCacheLv2() {
+        SqlSession sqlSession1 = factory.openSession();
+        UserDao mapper = sqlSession1.getMapper(UserDao.class);
+        User u = mapper.findById(2);
+        sqlSession1.close();
+
+        SqlSession sqlSession2 = factory.openSession();
+        UserDao mapper2 = sqlSession2.getMapper(UserDao.class);
+        User u2 = mapper2.findById(2);
+        sqlSession2.close();
         System.out.println(u);
         System.out.println(u2);
     }
